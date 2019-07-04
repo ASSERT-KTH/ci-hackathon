3e9575763b7339f7a4769cb733b17f087851a9f5
Javier Cabrera <javierca@kth.se>
2019-07-04 16:59:57 +0200

    WIP

--- cabrera_baudry/bundle.js
+++ cabrera_baudry/bundle.js
@@ -7,14 +7,17 @@ const Tone = require("./libs/tone.js");
 
 
 ws = new WebSocket('wss://travis.durieux.me');
-const maxNumberTracks = 25; //maximum number of tracks (CI jobs) that we listen to in parallel
-const synthPool = []; //array of synth used as a pool to play CI jobs
+const maxNumberTracks = 26; //maximum number of tracks (CI jobs) that we listen to in parallel
 var globalCount = 0; //this counter keeps increasing and records the total number of jobs that have been played since page load
 var playingJob = {} //keeps the list of sha values for each job being played. inv: playingJob.length < maxNumberTracks
+
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
-window.onload = initSynthPool();
+const debug = document.getElementById("debug");
+const debugContext = debug.getContext("2d");
+
+
 function start() {
     console.log("Starting");
     ws.onmessage = function (event) {
@@ -27,60 +30,11 @@ function start() {
     
 }
 
-function initSynthPool() {
-    for (var i = 0; i < maxNumberTracks; i++) {
-        synthPool[i] = [];
-        synthPool[i][0] = new Tone.Synth({
-            oscillator: {
-                type: 'triangle8'
-            },
-            envelope: {
-                attack: 2,
-                decay: 1,
-                sustain: 0.4,
-                release: 4
-            }
-        }).toMaster();
-        synthPool[i][1] = "no sha"; // each synth is initially available
-        console.log(synthPool[i][0]);
-        console.log(synthPool[i][1]);
-    }
-
-
-}
-
-function assignSynth(sha) {
-    console.log("look for a synth");
-    for (var i = 0; i < synthPool.length; i++) {
-        if (synthPool[i][1] === "no sha") {
-            synthPool[i][1] = sha;
-            //document.write("<p>".concat("Synth ").concat(i).concat(" is assigned to job ").concat(sha).concat("</p>"));
-            return synthPool[i][0];
-        }
-    }
-}
-
-function releaseSynth(sha) {
-    console.log("release a synth");
-    for (var i = 0; i < synthPool.length; i++) {
-        if (synthPool[i][1] === sha) {
-            synthPool[i][0].triggerRelease();
-            synthPool[i][0].triggerAttackRelease('F#3', '4n');
-            synthPool[i][1] = "no sha";
-            //document.write("<p>".concat("Synth ").concat(i).concat(" is released from job ").concat(sha).concat("</p>"));
-        }
-    }
-}
-
-function printArray(array) {
-    console.log(array);
-}
-
 //for the moment we consider only two situations: the job starts and we play; the job stops and we stop playing
 //we should consider more: alter the sound when the job is updated (but not finished); play something different depending on the final state of the job (errored, failed, passed)
 
 function handleJob(message) {
-    if (message.data.state === "started" && Object.keys(playingJob).length < maxNumberTracks && !(message.data.commit.sha in playingJob)) {
+    if (message.data.state === "started" && Object.keys(playingJob).length <= maxNumberTracks && !(message.data.commit.sha in playingJob)) {
         playJob(message);
         console.log("Started", message);
     }
@@ -95,48 +49,121 @@ function handleJob(message) {
     }
 }
 
+function drawCircle(x, y, radius, context, fillColor, strokeColor){
+
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = fillColor;
+    context.strokeStyle = strokeColor;
+    context.fill();
+    context.stroke();
+}
+
 function drawCanvas(){
 
     context.clearRect(0, 0, canvas.width, canvas.height);
-    
 
     for(const key in playingJob){
 
         const info  = playingJob[key];
-        console.log("drawing canvas", info.starting, info.counter);
+        //console.log("drawing canvas", info.starting, info.counter);
 
-        context.moveTo(0, info.starting);
-        context.lineTo(0.1*info.counter, info.starting);
+       drawCircle(info.starting[0], info.starting[1], info.counter, context, 'white', 'black');
+    
+    }
+    
+}
+
+function getJob(index){
+
+    for(key in playingJob)
+        if(playingJob[key].index === index)
+            return playingJob[key];
+    
+    return null;
+}
+
+function drawDebug(){
+
+    let square = parseInt(Math.sqrt(maxNumberTracks));
+    
+    debugContext.clearRect(0, 0, debug.width, debug.height);
+
+    console.log(debug.width);
+    let squareSizeW = parseInt(debug.width/square);
+    let squareSizeH = parseInt(debug.height/(square + (maxNumberTracks%square == 0? 0 : 1)));
+    let padding = 20;
+    for(let i = 0; i < maxNumberTracks; i++){
+
+        let x = i%square;
+        let y = parseInt(i/square);
+
+        // Draws the grid
+        debugContext.strokeRect(squareSizeW*x, squareSizeH*y, squareSizeW, squareSizeH)
+
+        const job = getJob(i);
+
+        if(job){
+            console.log(playingJob, job)
+            // Write the note
+            debugContext.fillText(job.playingNote, squareSizeW*(x) + padding, squareSizeH*(y + 1) - padding);
+        }
     }
 
-    context.stroke();
+
+    debugContext.stroke();
+
 }
+
+let index = 0;
+let maxSizeWave = 100;
 
 function playJob(message) {
     //add the job in the list of jobs being played
     const key = message.data.commit.sha;
 
+    let newIndex = index;
+
+    index = (index + 1)%maxNumberTracks;
     playingJob[key] = {
         interval: null,
         counter: 0,
-        starting: 400*Math.random()
+        index: newIndex,
+        starting: [ Math.random()*canvas.width, Math.random()*canvas.height], // 2d random space point in canvas
+        playingNote: '',
+        synth: new Tone.Synth({
+            oscillator: {
+                type: 'triangle8'
+            },
+            envelope: {
+                attack: 2,
+                decay: 1,
+                sustain: 0.4,
+                release: 4
+            }
+        }).toMaster()
     };
 
     playingJob[key].interval = setInterval(function() {
-        playingJob[key].counter++;
+        playingJob[key].counter = (playingJob[key].counter + 1)%maxSizeWave;
         drawCanvas();
     }, 10);
 
+
     //assign a synth to the sha of the input message
-    const synth = assignSynth(message.data.commit.sha);
+    const synth = playingJob[key].synth;
+
     if (synth != null) {
         //have the synth play the soundthat corresponds to the job
         const sound = soundForJob(message)
+        playingJob[key].playingNote = sound;
         synth.triggerAttack(sound);
         //document.write("<p>".concat(message.data.commit.sha).concat(" plays ").concat(sound).concat(". We are listening to ").concat(Object.keys(playingJob).length).concat(" sounds</p>"));
     } else {
         //document.write("<p>".concat(message.data.commit.sha).concat(" has no synth").concat("</p>"))
     }
+
+    drawDebug();
 }
 
 function stopPlayJob(message) {
@@ -145,11 +172,17 @@ function stopPlayJob(message) {
 
     console.log(key, playingJob[key]);
     
+    const synth = playingJob[key].synth;
+
+    //playing special sound for the end of the job
+    synth.triggerRelease();
+    synth.triggerAttackRelease('F#3', '4n');
+            
     clearInterval(playingJob[key].interval);
 
     delete playingJob[key];
-    //release the synth that was assigned to this job
-    releaseSynth(message.data.commit.sha);
+
+    drawDebug();
     //document.write("<p>".concat(message.data.commit.sha).concat(" is done: ").concat(message.data.state).concat(".We listened to ").concat(Object.keys(playingJob).length).concat(" sounds.</p>"));
 }
 
@@ -187,21 +220,36 @@ function soundForJob(message) {
     return 'D4';
 }
 
+first = true;
+
 function changeSize(message) {
-    const diffURL = message.data.commit.compare_url.concat(".diff");
-    console.log(diffURL);
-    fetch(diffURL, { mode: 'no-cors' })
-        .then(function (data) {
-            console.log(data);
-        })
-        .catch(function (error) {
-            console.log("could not fetch diff for ".concat(message.data.commit.sha));
-        });
+
+    if(first){
+        const diffURL = message.data.commit.compare_url.concat(".diff");
+        fetch(`http://localhost:8000/?url=${diffURL}`, {method: 'GET'})
+            .then(function (data) {
+                data.json().then(json => {
+                    console.log(json);
+                }).
+                catch(err => {
+
+                        console.log(err);
+                    }
+                );
+            })
+            .catch(function (error) {
+                console.log("could not fetch diff for ".concat(message.data.commit.sha));
+            });
+
+            //first = false;
+
+           
+    }
 }
 
 // Entrypoint
 document.addEventListener('DOMContentLoaded', function(){ // When page is completly loaded
-
+    drawDebug();
     document.getElementById("start-btn").addEventListener("click", start ,false); // Add button click event
 }, false);
 },{"./libs/tone.js":1}]},{},[2]);
