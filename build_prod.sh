7c6fddedcf30c2d06b246d42e007c0d4a0792976
Javier Cabrera <javierca@kth.se>
2019-09-19 07:49:40 +0200

    HOTFIX

--- build_prod.sh
+++ build_prod.sh
@@ -3,5 +3,6 @@ python3 site_generator/generate.py https://kth.github.io/ci-hackathon
 bundle exec jekyll build --config _config_build.yml
 
 git push origin --delete gh-pages
+touch build/_config.yml
 git add build && git commit -m "WIP"
 git subtree push  --prefix build origin gh-pages
