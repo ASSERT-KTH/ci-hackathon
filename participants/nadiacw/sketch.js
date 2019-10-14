5bab094f6592a268f26ee592c3e14a16977b60c1
Nadia <ncampowoytuk@gmail.com>
2019-10-14 21:59:46 +0200

    update sketch and clean up

--- participants/nadiacw/sketch.js
+++ participants/nadiacw/sketch.js
@@ -1,23 +1,18 @@
 let name;
 let drawM, drawF;
+let t= 0;
 
 travisListener.connect();
 travisListener.on(data => {
   name = data.commit.author_name
-
-  // console.log(name)
-
-  // if (data.commit.user) {
-  //   console.log(data.commit.user.name)
-  // }
-  var firstName = name.split(" ")[0].trim().toLowerCase(); 
+  var firstName = name.split(" ")[0].trim().toLowerCase();
   // console.log(firstName)
   // console.log("Contains a male name: " + mnames.includes(firstName));
-  if(mnames.includes(firstName)){
+  if (mnames.includes(firstName)) {
     drawM = true;
   }
   // console.log("Contains a female name: " + fnames.includes(firstName));
-  if(fnames.includes(firstName)){
+  if (fnames.includes(firstName)) {
     drawF = true;
     console.log(firstName)
   }
@@ -26,7 +21,7 @@ travisListener.on(data => {
 
 function setup() {
   preload();
-  createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
@@ -34,26 +29,63 @@ function draw() {
   fill(0, 102, 153);
   noStroke();
 
-  if(drawM){
-    fill(255,230,30)
-    ellipse(random(width), random(height), 55, 55);
+  // Draw for each name
+  if (drawM) {
+    let colorM = color(randomColor({
+      hue: 'yellow',
+      luminosity: 'light'
+    }));
+    fill(colorM);
+    // ellipse(random(width), random(height), 100, 100);
+    blob(100, random(width), random(height), 0.75, t);
     drawM = false;
   }
 
-  if(drawF){
-    fill(150,100,240)
-    ellipse(random(width), random(height), 55, 55);
+  if (drawF) {
+    let colorF = color(randomColor({
+      hue: 'purple',
+      luminosity: 'light'
+    }));
+    fill(colorF);
+    // ellipse(random(width), random(height), 100, 100);
+    blob(100, random(width), random(height), 0.75, t);
     drawF = false;
   }
 
-  // text(name, 10, 10, 1000, 1000);
+
+  t += 0.5;
 }
 
-// Male and female names
+// blob code from https://www.openprocessing.org/sketch/744449
+function blob(size, xCenter, yCenter, k, t) {
+  beginShape();
+  for (let theta = 0; theta < 2 * PI; theta += 0.01) {
+    let r1, r2;
+    if (theta < PI/2) {
+      r1 = cos(theta);
+      r2 = 1;
+    } else if (theta < PI) {
+      r1 = 0;
+      r2 = sin(theta);
+    } else if (theta < 3 * PI/2) {
+      r1 = sin(theta);
+      r2 = 0;
+    } else {
+      r1 = 1;
+      r2 = cos(theta);
+    }
+    let r = size + noise(k * r1, k * r2, t) * (1/3) * size;
+    let x = xCenter + r * cos(theta);
+    let y = yCenter + r * sin(theta);
+    curveVertex(x, y);
+  }
+  endShape();
+}
+
+// Male and female name databases. Obtained from https://data.world/howarder/gender-by-name
 let fnames, mnames;
 function preload() {
   fnames = loadStrings('fnames.txt');
   mnames = loadStrings('mnames.txt');
-  // console.log(fnames)
 }
 
