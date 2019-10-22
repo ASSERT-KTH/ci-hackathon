1b55d476a0794d515b2af861306cd9d31a4b7897
tdurieux <tdurieux@users.noreply.github.com>
2019-10-22 13:30:18 +0000

    Automated deployment: Tue Oct 22 13:30:18 UTC 2019 3260a0340d0abeeca0d64f3c687f780c7a66d838

--- solutions/tdurieux_poet_history/script.mjs
+++ solutions/tdurieux_poet_history/script.mjs
@@ -1,9 +1,25 @@
-import { type } from 'https://unpkg.com/@camwiegert/typical';
+import { type } from './typical.js';
+
+function changeTimezone(date, ianatz) {
+    return date;
+    // suppose the date is 12:00 UTC
+    var invdate = new Date(date.toLocaleString('en-GB', {
+      timeZone: ianatz
+    }));
+    console.log(invdate)
+  
+    // then invdate will be 07:00 in Toronto
+    // and the diff is 5 hours
+    var diff = date.getTime() - invdate.getTime();
+  
+    // so 12:00 in Toronto is 17:00 UTC
+    return new Date(date.getTime() - diff);
+}
 
 (async () => {
     const lines = []
 
-    async function write(text) {
+    async function write(t, text) {
         const promises = []
         const s = text.split('\n')
         for (let i = 0; i< s.length; i ++) {
@@ -12,11 +28,26 @@ import { type } from 'https://unpkg.com/@camwiegert/typical';
                 lines[i] = s[i]
             }
         }
+        const previous = document.querySelector('.time.active')
+        if (previous) {
+            previous.className = 'time'
+        }
+        document.getElementById('time-'+ t).className = 'time active'
         await Promise.all(promises)
     }
-    await write(poets[0])
-    for (let i = 0; i< poets.length; i ++) {
-        document.getElementById("id").innerText = i
-        await write(poets[i])
+    
+    let content = '';
+    for (let t of dates) {
+        const d = new Date(t);
+        // d.getDate()  + "-" + (d.getMonth()+1) + "-" + d.getFullYear() + " " +
+        const datestring = ("0" + d.getHours()).slice(-2) + "h" + ("0" + d.getMinutes()).slice(-2);
+
+        content += '<div id="time-' + t + '" class="time" onclick="activate(this)" data-value="' + t + '"><span>' + datestring + '</span></div>'
+    }
+    time_travel.innerHTML = content;
+    await write(dates[0], poems[dates[0]])
+    for (index; index < dates.length; index ++) {
+        document.getElementById("id").innerText = index
+        await write(dates[index], poems[dates[index]])
     }
 })()
