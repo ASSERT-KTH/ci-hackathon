28ccb459e5a960e207ffd610d4eb55e65adb4947
tdurieux <durieuxthomas@hotmail.com>
2019-10-23 17:03:05 -0400

    cleanup the script

--- participants/tdurieux/poet_history/typical.js
+++ participants/tdurieux/poet_history/typical.js
@@ -1,55 +1,57 @@
-export async function type(node, ...args) {
-    for (const arg of args) {
-        switch (typeof arg) {
-            case 'string':
-                await edit(node, arg);
-                break;
-            case 'number':
-                await wait(arg);
-                break;
-            case 'function':
-                await arg(node, ...args);
-                break;
-            default:
-                await arg;
+(() => {
+    window.type = async function (node, ...args) {
+        for (const arg of args) {
+            switch (typeof arg) {
+                case 'string':
+                    await edit(node, arg);
+                    break;
+                case 'number':
+                    await wait(arg);
+                    break;
+                case 'function':
+                    await arg(node, ...args);
+                    break;
+                default:
+                    await arg;
+            }
         }
     }
-}
-
-async function edit(node, text) {
-    const overlap = getOverlap(node.textContent, text);
-    await perform(node, [...deleter(node.textContent, overlap), ...writer(text, overlap)]);
-}
-
-async function wait(ms) {
-    await new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function perform(node, edits, speed = 15) {
-    for (const op of editor(edits)) {
-        op(node);
-        await wait(speed + speed * (Math.random() - 0.5));
+    
+    async function edit(node, text) {
+        const overlap = getOverlap(node.textContent, text);
+        await perform(node, [...deleter(node.textContent, overlap), ...writer(text, overlap)]);
     }
-}
-
-export function* editor(edits) {
-    for (const edit of edits) {
-        yield (node) => requestAnimationFrame(() => node.textContent = edit);
+    
+    async function wait(ms) {
+        await new Promise(resolve => setTimeout(resolve, ms));
     }
-}
-
-export function* writer([...text], startIndex = 0, endIndex = text.length) {
-    while (startIndex < endIndex) {
-        yield text.slice(0, ++startIndex).join('');
+    
+    async function perform(node, edits, speed = 15) {
+        for (const op of editor(edits)) {
+            op(node);
+            await wait(speed + speed * (Math.random() - 0.5));
+        }
     }
-}
-
-export function* deleter([...text], startIndex = 0, endIndex = text.length) {
-    while (endIndex > startIndex) {
-        yield text.slice(0, --endIndex).join('');
+    
+    function* editor(edits) {
+        for (const edit of edits) {
+            yield (node) => requestAnimationFrame(() => node.textContent = edit);
+        }
     }
-}
-
-export function getOverlap(start, [...end]) {
-    return [...start, NaN].findIndex((char, i) => end[i] !== char);
-}
+    
+    function* writer([...text], startIndex = 0, endIndex = text.length) {
+        while (startIndex < endIndex) {
+            yield text.slice(0, ++startIndex).join('');
+        }
+    }
+    
+    function* deleter([...text], startIndex = 0, endIndex = text.length) {
+        while (endIndex > startIndex) {
+            yield text.slice(0, --endIndex).join('');
+        }
+    }
+    
+    function getOverlap(start, [...end]) {
+        return [...start, NaN].findIndex((char, i) => end[i] !== char);
+    }
+})();
